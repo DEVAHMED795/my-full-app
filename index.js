7091b6cedf120368e52ee1235dc0438cfa27c56d
DEVAHMED795 <nazhiahmed32@gmail.com>
2026-04-26 21:19:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ const { createClient } = require("@libsql/client");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
-// Cloud Database Setup
-const db = createClient({
-  url: process.env.TURSO_DB_URL,
-  authToken: process.env.TURSO_DB_AUTH_TOKEN,
-});
+app.use(cors({
+  origin: ["https://my-full-app-2ujc.vercel.app", "http://localhost:5173"],
+  credentials: true
+}));
 
 // Function to create the table if it doesn't exist
 async function setupDatabase() {
